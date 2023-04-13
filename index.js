3f9986fe74bba128e27e11378cc6692a19727f82
VictorCalisto <83865085+VictorCalisto@users.noreply.github.com>
2023-04-12 21:16:25 -0300

    deploy 12

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 
-// configurar o Express.js para servir arquivos estáticos da pasta "public"
-app.use(express.static(path.join(__dirname, 'public')));
+// Definir caminhos para arquivos estáticos
+app.use(express.static(path.join(__dirname, 'HTML')));
+app.use(express.static(path.join(__dirname, 'CSS')));
+app.use(express.static(path.join(__dirname, 'JS')));
 
-// configurar o Express.js para servir o arquivo "index.html" na raiz do aplicativo
+// Definir rota para o arquivo index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'HTML', 'index.html'));
 });
 
-// iniciar o servidor HTTP na porta 3000
-app.listen(process.env.PORT || 3000);
+// Definir porta do servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
