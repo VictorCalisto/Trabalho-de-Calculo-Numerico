5bdf166114b703e0fe78a57bafb75979f89c9b89
VictorCalisto <83865085+VictorCalisto@users.noreply.github.com>
2023-04-12 22:51:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/index.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/HTML/calculadora', (req, res) => {
+  res.sendFile(path.join(__dirname, 'HTML', 'calculadora.html'));
+});
+
+app.get('/bisseccao', (req, res) => {
+  res.sendFile(path.join(__dirname, 'HTML', 'bisseccao.html'));
+});
+
+app.get('/newton', (req, res) => {
+  res.sendFile(path.join(__dirname, 'HTML', 'newton.html'));
+});
+
+app.get('/cordas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'HTML', 'cordas.html'));
+});
+
+app.get('/secante', (req, res) => {
+  res.sendFile(path.join(__dirname, 'HTML', 'secante.html'));
 });
 
 // Definir porta do servidor
