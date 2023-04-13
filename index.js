d76ee52eeafe1061452d98b6f4dcb3457af2719e
VictorCalisto <83865085+VictorCalisto@users.noreply.github.com>
2023-04-12 21:08:03 -0300

    deploy 11

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
+// configurar o Express.js para servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
 // configurar o Express.js para servir o arquivo "index.html" na raiz do aplicativo
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // iniciar o servidor HTTP na porta 3000
