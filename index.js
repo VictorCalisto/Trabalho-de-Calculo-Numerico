517280fb3b63870bfeac687b12d4089de05caadb
VictorCalisto <83865085+VictorCalisto@users.noreply.github.com>
2023-04-12 21:18:51 -0300

    deploy 14

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'JS')));
 
 // Definir rota para o arquivo index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html');
 });
 
 // Definir porta do servidor
