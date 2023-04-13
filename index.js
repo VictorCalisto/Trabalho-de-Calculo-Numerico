865ed20bd2b833d55ee119df278ff50ac59e9dc5
VictorCalisto <83865085+VictorCalisto@users.noreply.github.com>
2023-04-12 21:44:09 -0300

    deploy 16

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express();
 const path = require('path');
 
 // Definir caminhos para arquivos estáticos
-app.use(express.static(path.join(__dirname, 'HTML')));
 app.use(express.static(path.join(__dirname, 'CSS')));
 app.use(express.static(path.join(__dirname, 'JS')));
 
 // Definir rota para o arquivo index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Definir rota para outras páginas HTML
+app.get('/:pagina.html', (req, res) => {
+  const pagina = req.params.pagina;
+  const arquivo = path.join(__dirname, 'HTML', `${pagina}.html`);
+  res.sendFile(arquivo);
 });
 
 // Definir porta do servidor
